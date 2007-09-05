73c283aa03e138dd92dc78675374fc492ddec0b1
ruyasan <ruyasan@94fdf3fb-5e37-0410-bc4e-df5ac2b4ea7b>
2007-09-05 19:04:11 +0000

    added ability to simply pass a rep_name, fixed bug with sass import

--- ojs/lib/ojs/html_representation/view_helpers.rb
+++ ojs/lib/ojs/html_representation/view_helpers.rb
@@ -10,6 +10,9 @@ module OJS
       # The +for_object+ must be a hash-like (responds to :[]) and have an :id key. (fun fact: ActiveRecord objects all do)
       # If nil, the +for_object+ is set to {:id=>'N'}
       #
+      # You can also pass a symbol as the for_object, in which case it will be treated as nil, and both the representation_name
+      # and the object_class will be set to it.
+      #
       # The +representation_name+ is used to generate the html ids - for instance, a name of :comment will generate
       # IDs like "comment_23" and attribute ids like "comment_23_author". If blank, it is guessed to be the 'same' as the
       # partial name. (i.e. _comment.rhtml will assume :comment as the representation_name)
@@ -26,17 +29,23 @@ module OJS
       def rep(for_object = nil, representation_name = nil, object_class = nil)
         path_to_calling_view = caller.find{|p| p.starts_with?(base_path)}[/[^:]+/].split(File::SEPARATOR)
         
+        if for_object.is_a?(Symbol)
+          representation_name = for_object
+          object_class = for_object
+          for_object = nil 
+        end
+        
         representation_name ||= path_to_calling_view[-1][/_?([^.]+)/,1]
 
         object_class ||= 
-                if    for_object.nil? || for_object.is_a?(Hash)
+                if for_object.nil? || for_object.is_a?(Hash) 
                   path_to_calling_view[-2].singularize
                 elsif for_object.respond_to? :to_ary
                   for_object.first.class.name.underscore
                 else
                   for_object.class.name.underscore
                 end
-
+                
         Representation.find_or_create(representation_name.to_s, object_class.to_s, for_object, self)
       end
     end
--- ojs/lib/ojs/loader/sass_resource.rb
+++ ojs/lib/ojs/loader/sass_resource.rb
@@ -1,11 +1,25 @@
 require 'haml'
+# Fix stupidity
+module Sass
+  module Tree
+    class ValueNode
+      def initialize(value, style)
+        if value =~ /^\@import (.*)/
+          value = "@import url(/stylesheets/#{$1});"
+        end
+        @value = value
+        super(style)
+      end
+    end
+  end
+end
 module OJS
   module Loader
     class SassResource < DefaultResource
       def content
         unless  @compiled_css && @last_compile > file_path.mtime
           time = Benchmark.measure do
-            @compiled_css = Sass::Engine.new(source).render
+            @compiled_css = Sass::Engine.new(source, :filename => file_path.to_s, :load_paths=>[File.join(RAILS_ROOT, 'public','stylesheets')]).render
             @last_compile = Time.now
           end
           RAILS_DEFAULT_LOGGER.info "Compiled SASS #{name} in #{"%.4fs" % time.real}."
